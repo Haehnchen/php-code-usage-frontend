eeed364cf97316ed47c749d27f27d1caa46b7df8
Daniel Espendiller <daniel@espendiller.net>
2014-07-22 20:00:30 +0200

    set http context in command

--- src/espend/Inspector/FrontendBundle/Command/SitemapCommand.php
+++ src/espend/Inspector/FrontendBundle/Command/SitemapCommand.php
@@ -13,6 +13,11 @@ class SitemapCommand extends ContainerAwareCommand {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
+
+        $context = $this->getContainer()->get('router')->getContext();
+        $context->setHost('code-usage.com');
+        $context->setScheme('http');
+
         $filename = dirname($this->getContainer()->get('kernel')->getRootDir()) . '/web/sitemap.xml';
         $content = $this->getContainer()->get('espend_inspector_frontend.sitemap_generator')->getContent();
         file_put_contents($filename, $content);
